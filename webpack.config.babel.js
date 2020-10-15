46e6d01aa672e00f1fa5e33b508aeb898a6ae53e
f1eeman <mr.f1eeman@gmail.com>
2020-10-16 01:23:54 +0600

    feat: add server

---
+++ webpack.config.babel.js
@@ -0,0 +1,47 @@
+// @ts-check
+
+import path from 'path';
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const isProduction = process.env.NODE_ENV === 'production';
+// const isDevelopment = !isProduction;
+console.log('isProduction', isProduction);
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  externals: {
+    gon: 'gon',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  output: {
+    path: `${__dirname}/dist/public`,
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist', 'public'),
+    // publicPath: '/assets/',
+    contentBasePublicPath: '/assets/',
+    compress: true,
+  },
+  plugins: [
+    // new MiniCssExtractPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'postcss-loader' },
+          { loader: 'sass-loader' },
+        ],
+      },
+    ],
+  },
+};
