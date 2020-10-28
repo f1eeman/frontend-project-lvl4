9b1ca90bf6956114a8c9a9767188f7b8ff8b75b2
f1eeman <mr.f1eeman@gmail.com>
2020-10-29 01:19:43 +0600

    refactor: delete key.js

--- src/key.js
+++
@@ -1,8 +0,0 @@
-import aws from 'aws-sdk';
-
-const rollbarKey = new aws.S3({
-  accessKeyId: process.env.SECRET_KEY_ROLLBAR_KEY,
-  secretAccesKey: process.env.SECRET_KEY_ROLLBAR_SECRET,
-});
-
-export default rollbarKey;
